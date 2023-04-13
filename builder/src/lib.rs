bacacef30a0432ed71fb3482a2edb0175bc37193
UserIsntAvailable <pinedap.angel@gmail.com>
2023-04-13 09:59:22 -0400

    refactor(builder): clean up.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,95 +1,109 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed, Ident, Type};
+
+/// Information about the fields that are going to be generated.
+struct GenField {
+    ident: Ident,
+    ty: Type,
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let DeriveInput { ident, data, .. } = parse_macro_input!(input as DeriveInput);
+    let fields = if let Data::Struct(DataStruct {
+        fields: Fields::Named(FieldsNamed { named, .. }),
+        ..
+    }) = data
+    {
+        named
+            .into_iter()
+            .map(|field| GenField {
+                ident: field.ident.expect("named fields"),
+                ty: field.ty,
+            })
+            .collect::<Vec<_>>()
+    } else {
+        unreachable!()
+    };
 
     let builder_ident = format_ident!("{ident}Builder");
-    let (struct_fields, init_fields, setters, build_method_init_fields) =
-        if let Data::Struct(DataStruct {
-            fields: Fields::Named(FieldsNamed { named, .. }),
-            ..
-        }) = data
-        {
-            // TODO: Is it a good practice to directly put an `Option` inside `quote!` if I now they
-            // can't be `None`?
-            // TODO: Split different sections on different functions.
-            // TODO: return `named` from `if let` to remove unnecessary nesting.
-            // FIX: Stop collecting `vectors` and use the `#(...)*` syntax to use iterators.
-
-            let struct_fields = named
-                .iter()
-                .map(|Field { ident, ty, .. }| {
-                    quote! { #ident: std::option::Option<#ty>, }
-                })
-                .collect::<TokenStream>();
-
-            let init_fields = named
-                .iter()
-                .map(|Field { ident, .. }| {
-                    quote! { #ident: None, }
-                })
-                .collect::<TokenStream>();
-
-            let setters = named
-                .iter()
-                .map(|Field { ident, ty, .. }| {
-                    quote! {
-                        fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                            self.#ident = Some(#ident);
-                            self
-                        }
-                    }
-                })
-                .collect::<TokenStream>();
-
-            let build_method_init_fields = named
-                .iter()
-                .map(|Field { ident, .. }| {
-                    let error_msg =
-                        format!("The field `{}` was not setted.", ident.as_ref().unwrap());
-                    quote! {
-                        #ident: self.#ident
-                            .take()
-                            .ok_or_else(|| String::from(#error_msg))?,
-                    }
-                })
-                .collect::<TokenStream>();
-
-            (
-                struct_fields,
-                init_fields,
-                setters,
-                build_method_init_fields,
-            )
-        } else {
-            unreachable!()
-        };
+    let builder_struct = gen_builder_struct(&builder_ident, &fields);
+    let buildee_impl = gen_buildee_impl(&ident, &builder_ident, &fields);
+    let builder_impl = gen_builder_impl(&ident, &builder_ident, &fields);
 
     quote! {
-        pub struct #builder_ident {
-            #struct_fields
-        }
+        #builder_struct
+        #buildee_impl
+        #builder_impl
+    }
+    .into()
+}
 
-        impl #ident {
+fn gen_builder_struct(ident: &Ident, fields: &[GenField]) -> TokenStream {
+    let fields = fields.iter().map(|GenField { ident, ty, .. }| {
+        quote! { #ident: std::option::Option<#ty> }
+    });
+
+    quote! {
+        pub struct #ident {
+            #(#fields),*
+        }
+    }
+}
+
+fn gen_buildee_impl(
+    buildee_ident: &Ident,
+    builder_ident: &Ident,
+    fields: &[GenField],
+) -> TokenStream {
+    let init_struct_fields = fields.iter().map(|GenField { ident, .. }| {
+        quote! { #ident: None }
+    });
+
+    quote! {
+        impl #buildee_ident {
             pub fn builder() -> #builder_ident {
                 #builder_ident {
-                    #init_fields
+                    #(#init_struct_fields),*
                 }
             }
         }
+    }
+}
 
+fn gen_builder_impl(
+    buildee_ident: &Ident,
+    builder_ident: &Ident,
+    fields: &[GenField],
+) -> TokenStream {
+    let setters = fields.iter().map(|GenField { ident, ty, .. }| {
+        quote! {
+            fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        }
+    });
+
+    let init_struct_fields = fields.iter().map(|GenField { ident, .. }| {
+        let error_msg = format!("The field `{}` was not setted.", ident);
+        quote! {
+            #ident: self.#ident
+                .take()
+                .ok_or_else(|| String::from(#error_msg))?
+        }
+    });
+
+    quote! {
         impl #builder_ident {
-            #setters
+            #(#setters)*
 
-            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
-                std::result::Result::Ok(#ident {
-                    #build_method_init_fields
+            pub fn build(&mut self) -> Result<#buildee_ident, Box<dyn std::error::Error>> {
+                std::result::Result::Ok(#buildee_ident {
+                    #(#init_struct_fields),*
                 })
             }
         }
     }
-    .into()
 }
